5905dfe2d00b83b265c660ac4a954f650d9a263a
John C. Lavelle <jlavelle604@gmail.com>
2019-04-29 01:21:08 -0400

    ProgView type for viewing FaunBrick as a list

--- src/FaunBrick/AST.hs
+++ src/FaunBrick/AST.hs
@@ -1,12 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module FaunBrick.AST (
-  FaunBrick(..),
-  FaunBrickF(..),
-  Instruction(..),
-  Program
-) where
+module FaunBrick.AST where
 
+import GHC.Exts (IsList(..))
 import GHC.Generics (Generic, Generic1)
 import Control.DeepSeq (NFData, NFData1)
 
@@ -31,6 +27,28 @@ instance Semigroup (FaunBrick a) where
 
 instance Monoid (FaunBrick a) where
   mempty = Halt
+
+data SumR f a = L a | R (f a)
+  deriving (Functor, Foldable, Traversable, Eq, Show)
+
+newtype ProgView a = ProgView { runProgView :: [SumR ProgView a] }
+  deriving (Functor, Foldable, Traversable, Eq, Show)
+
+instance IsList (FaunBrick a) where
+  type Item (FaunBrick a) = SumR ProgView a
+  toList Halt = []
+  toList (Instr a r)  = L a : toList r
+  toList (Loop as bs) = R (ProgView $ toList as) : toList bs
+
+  fromList [] = Halt
+  fromList (L a : xs) = Instr a $ fromList xs
+  fromList (R v : xs) = Loop (fromList $ runProgView v) $ fromList xs
+
+toProgView :: FaunBrick a -> ProgView a
+toProgView = ProgView . toList
+
+fromProgView :: ProgView a -> FaunBrick a
+fromProgView = fromList . runProgView
 
 data Instruction
   = Forward
