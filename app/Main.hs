3cf8e62a65e06e32a701633e4518cbacfb83000b
John C. Lavelle <jlavelle604@gmail.com>
2019-05-02 17:58:16 -0400

    Run mandel.b with successive optimizations

--- app/Main.hs
+++ app/Main.hs
@@ -5,14 +5,18 @@ import Control.Monad (void)
 import qualified Data.Text.Lazy.IO as T
 import qualified Data.Text.Lazy.Builder as B
 import Data.Time (getCurrentTime, diffUTCTime)
+import Data.Monoid (Endo(..))
+import Data.Foldable (fold, traverse_)
+import Data.List (inits)
 
 import FaunBrick.Interpreter (interpretIO', interpretPure')
 import FaunBrick.Parser (parseFile)
-import FaunBrick.AST.Optimize (optimize)
+import FaunBrick.AST.Optimize
+import FaunBrick.AST (Program)
 import FaunBrick.Interpreter.Types (TextHandle(..))
 
 runFile :: FilePath -> IO ()
-runFile p = void $ optimize <$> parseFile p >>= interpretIO'
+runFile p = void $ optimize <$> parseFile p >>= flip timed p
 
 runFilePure :: FilePath -> IO ()
 runFilePure p = void $ interpretPure' . optimize <$> parseFile p >>= f
@@ -20,17 +24,36 @@ runFilePure p = void $ interpretPure' . optimize <$> parseFile p >>= f
     f (Right (_, t)) = T.putStrLn $ B.toLazyText $ textHandleOut t
     f (Left e) = error $ "runFilePure: Interpreter error " <> show e
 
+subOpts :: (forall a. [a] -> [[a]]) -> [(Int, Optimization)]
+subOpts f = zip [0..] opts
+  where
+    opts = fmap (eqFix . appEndo . fold) $ f $ fmap Endo
+      [ loopsToMul
+      , contract
+      , fuse
+      , elimClears
+      , offsets
+      , uninterpose
+      , loopsToIfs
+      , dedupMulSet
+      ]
+
+progOpts :: (forall a. [a] -> [[a]]) -> Program -> [(Int, Program)]
+progOpts f p = fmap (fmap ($ p)) $ subOpts f
+
 main :: IO ()
 main = do
-  putStrLn "Mandelbrot - IO"
+  p <- parseFile "programs/mandel.b"
+  traverse_ go $ progOpts inits p
+  where
+    go (i, p) = timed p $ "Mandelbrot with opt " <> show i
+
+
+timed :: Program -> String -> IO ()
+timed p s = do
+  putStrLn s
   t <- getCurrentTime
-  putStrLn $ "Started at " <> show t
-  runFile "programs/mandel.b"
+  putStrLn $ "Statred at " <> show t
+  interpretIO' p
   t' <- getCurrentTime
   putStrLn $ "Runtime: " <> show (diffUTCTime t' t)
-  putStrLn "Mandelbrot - Pure"
-  t'' <- getCurrentTime
-  putStrLn $ "Started at " <> show t''
-  runFilePure "programs/mandel.b"
-  t''' <- getCurrentTime
-  putStrLn $ "Runtime: " <> show (diffUTCTime t''' t'')
